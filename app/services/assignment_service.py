be3cd31ff506333f6586858784eb56b06741fcfb
Subhojit Das <subhojit05das@gmail.com>
2026-09-24 10:32:37 +0530

    Implement asset assignment logic

--- app/services/assignment_service.py
+++ app/services/assignment_service.py
@@ -1,5 +1,3 @@
-from app.services.employee_service import EmployeeService
-from app.services.asset_service import AssetService
 from app.utils.enums import AssetStatus
 
 class AssignmentService:
@@ -11,5 +9,13 @@ class AssignmentService:
         employee = self.employee_service.get_employee(employee_id)
         asset = self.asset_service.get_asset(asset_id)
 
+        if asset is None or employee is None:
+            return None
+
         if asset.status != AssetStatus.AVAILABLE:
             return None
+
+        employee.add_asset(asset)
+        asset.status = AssetStatus.ASSIGNED
+
+        return asset
--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 from app.services.employee_service import EmployeeService
 from app.services.asset_service import AssetService
 from app.utils.enums import AssetType, AssetStatus
+from app.services.assignment_service import AssignmentService
 
 service = EmployeeService()
 
 emp1 = service.create_employee(1001, "Aizen", "aizen@mail.com", "IT")
 emp2 = service.create_employee(1002, "Gin", "gin@mail.com", "Finance")
 
-for emp_id in service.employees.items():
-    print(emp_id)
-
-get_all = service.get_all_employees()
-for employee in get_all:
-    print(employee)
-
 new_service = AssetService()
 
-asset1 = new_service.create_asset(1, AssetType.LAPTOP, "DELL", "Dell Latitude 5124", "SN45678", AssetStatus.ASSIGNED)
+asset1 = new_service.create_asset(1, AssetType.LAPTOP, "DELL", "Dell Latitude 5124", "SN45678", AssetStatus.AVAILABLE)
 
-print(asset1)
+add_asset = AssignmentService(service, new_service)
 
-print(new_service.get_assets(1))
-
-assets = new_service.get_all_assets()
-for asset in assets:
-    print(asset)
+print(add_asset.assign_asset(1001, 1))
